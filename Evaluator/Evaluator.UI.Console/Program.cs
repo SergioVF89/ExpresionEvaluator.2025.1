99b2350ce4cb2d6ecdef3855b01178cd2c114834
SergioVF89 <fonnegrasergio@gmail.com>
2025-04-08 11:33:24 -0500

    Update

--- Evaluator/Evaluator.UI.Console/Program.cs
+++ Evaluator/Evaluator.UI.Console/Program.cs
@@ -5,7 +5,7 @@ try
     var resut2 = FunctionEvaluator.Evaluate("4*5/(4+6)");
     var resut1 = FunctionEvaluator.Evaluate("4*(5+6-(8/2^3)-7)-1");
     var resut3 = FunctionEvaluator.Evaluate("9^(1/2)");
-    var resut4 = FunctionEvaluator.Evaluate("100+25*(50-20)/10");
+    var resut4 = FunctionEvaluator.Evaluate("100+ 25*( 50 -20 ) / 10");
     Console.WriteLine(resut1);
     Console.WriteLine(resut2);
     Console.WriteLine(resut3);
